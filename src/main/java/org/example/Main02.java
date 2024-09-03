5fd6cc2e93acfe2a0b93c3eb4763373bd6beb8d5
Kimginam97 <rlarlska79@naver.com>
2024-09-03 20:52:03 +0900

    2번 배열제어하기 println

--- src/main/java/org/example/Main02.java
+++ src/main/java/org/example/Main02.java
@@ -5,11 +5,18 @@ import java.util.Collections;
 
 // 2번 배열 제어하기
 public class Main02 {
+
+    public static void main(String[] args){
+        System.out.println(Arrays.toString(solution(new int[]{4,2,2,1,3,4})));
+        System.out.println(Arrays.toString(solution(new int[]{2,1,1,3,2,5,4})));
+    }
+
+
     private static int[] solution(int[] arr){
         // 중복값 제거
         Integer[] result = Arrays.stream(arr).boxed().distinct().toArray(Integer[]::new);
 
-        System.out.println("중복값 제거: "+result);
+        System.out.println("중복값 제거: "+Arrays.toString(result));
 
         // 내림차순 정렬
         Arrays.sort(result, Collections.reverseOrder());
