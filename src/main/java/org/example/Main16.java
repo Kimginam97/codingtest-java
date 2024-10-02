b0800272364ee8e5ac6995e4e8ba4e0702023a4a
Kimginam97 <rlarlska79@naver.com>
2024-10-02 12:22:08 +0900

    16. 기능개발

---
+++ src/main/java/org/example/Main16.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+// 16. 기능개발
+public class Main16 {
+    public int[] solution(int[] progresses, int[] speeds){
+        Queue<Integer> answer = new ArrayDeque<>();
+
+        int n = progresses.length;
+
+        // 1. 각 작업의 배포 가능일 계산
+        int[] daysLeft = new int[n];
+        for(int i =0; i < n; i++){
+            daysLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        }
+
+        int count = 0; // 2. 배포될 작업의 수 카운트
+        int maxDay = daysLeft[0]; // 3. 현재 배포될 작업중 가장 늦게 배포될 작업의 기능일
+
+        for(int i = 0; i < n; i++){
+            if(daysLeft[i] <= maxDay){ // 4. 배포 가능일이 가장 늦은 배포일보다 빠르면
+                count++;
+            }else{// 5. 배포 예정일이 기준 배포일보다 느리면
+                answer.add(count);
+                count = 1;
+                maxDay = daysLeft[i];
+            }
+        }
+
+        answer.add(count); // 6. 마지막으로 카운트된 작업들을 함께 배포
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
